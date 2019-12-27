15ee993aa8c4faea1788f46b87d613e4d683dd88
pikaChin <derrfgh4563@gmail.com>
2019-12-27 14:44:42 +0800

    Add animation to the layout

--- app/src/main/java/com/example/animationprototype/MainActivity.java
+++ app/src/main/java/com/example/animationprototype/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.animationprototype;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout jumpOutLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jumpOutLayout = findViewById(R.id.jumpOutLayout);
+
+        findViewById(R.id.testBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.test_anim);
+                jumpOutLayout.setVisibility(View.VISIBLE);
+                jumpOutLayout.startAnimation(animation);
+            }
+        });
     }
 }
