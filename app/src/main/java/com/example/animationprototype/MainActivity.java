79228d2a4e0d6b6418363b2d44d28c84bedc21f1
pikaChin <derrfgh4563@gmail.com>
2019-12-29 20:33:00 +0800

    Use dialog to show the window

--- app/src/main/java/com/example/animationprototype/MainActivity.java
+++ app/src/main/java/com/example/animationprototype/MainActivity.java
@@ -3,29 +3,56 @@ package com.example.animationprototype;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    ConstraintLayout jumpOutLayout;
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+//    ConstraintLayout jumpOutLayout;
+    ListView testList;
+    Dialog myDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDialog = new Dialog(this);
 
-        jumpOutLayout = findViewById(R.id.jumpOutLayout);
+//        jumpOutLayout = findViewById(R.id.jumpOutLayout);
+        testList = findViewById(R.id.testList);
 
-        findViewById(R.id.testBtn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.test_anim);
-                jumpOutLayout.setVisibility(View.VISIBLE);
-                jumpOutLayout.startAnimation(animation);
-            }
-        });
+        ArrayList<String> list = new ArrayList<>();
+        for (int i=0; i<10; i++)
+            list.add("test" + i);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, list);
+        testList.setAdapter(adapter);
+        testList.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//        Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.test_anim);
+//        jumpOutLayout.setVisibility(View.VISIBLE);
+//        jumpOutLayout.bringToFront();
+//        jumpOutLayout.startAnimation(anim);
+        showPopup();
+    }
+
+    private void showPopup() {
+        myDialog.setContentView(R.layout.custom_dialog);
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
     }
 }
